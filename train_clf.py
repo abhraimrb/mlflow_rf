8cbea6bdb8cfdb76bfd2f35efae6e2ca23e4bffc
abhraimrb <30985012+abhraimrb@users.noreply.github.com>
2021-11-10 18:18:55 +0530

    Update train_clf.py

--- train_clf.py
+++ train_clf.py
@@ -32,9 +32,12 @@ if __name__ == "__main__":
     X_test = test.drop(["loan_approval_status"], axis=1)
     y_train = train[["loan_approval_status"]]
     y_test = test[["loan_approval_status"]]
-    n_estimators = int(sys.argv[1]) if len(sys.argv) > 1 else 100
-    min_samples_split = int(sys.argv[2]) if len(sys.argv) > 2 else 2
-    min_samples_leaf  = int(sys.argv[3]) if len(sys.argv) > 3 else 2
+    n_estimators =  100
+    min_samples_split = 2
+    min_samples_leaf  =  2    
+    #n_estimators = int(sys.argv[1]) if len(sys.argv) > 1 else 100
+    #min_samples_split = int(sys.argv[2]) if len(sys.argv) > 2 else 2
+    #min_samples_leaf  = int(sys.argv[3]) if len(sys.argv) > 3 else 2
     with mlflow.start_run():         
         model = RandomForestClassifier(n_estimators=n_estimators,min_samples_split=min_samples_split, 
                                        min_samples_leaf=min_samples_leaf)
